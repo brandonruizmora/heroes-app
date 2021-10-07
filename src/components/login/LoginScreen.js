3e163665875bc5abba3c64902e90cf251a4272a0
brandonruizmora <brandon_ruizmor@outlook.es>
2021-10-06 21:11:34 -0500

    added loginScreen

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { useHistory } from "react-router-dom";
 
 export const LoginScreen = () => {
+
+    let history = useHistory();
+
+    const handleLogin = () => {
+        // history.push('/');
+        history.replace('/');
+    }
+
     return (
-        <div>
-            Login Screen
+        <div className="container mt-5">
+            <h1>Login Screen</h1>
+            <hr />
+            <button 
+            className="btn btn-primary"
+            onClick={ handleLogin }
+            >Login</button>
         </div>
     )
 }

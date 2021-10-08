703958bda6874e3ef14e42692ea124b425eb4977
brandonruizmora <brandon_ruizmor@outlook.es>
2021-10-08 15:49:02 -0500

    Added hero cards

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { getHeroesByPublisher } from '../selectors/getHeroesByPublisher';
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
 
     const heroes = getHeroesByPublisher(publisher);
 
     return (
-        <ul>
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 row-cols-xl-8 g-8">
             {
                 heroes.map(heroe => {
                     return (
-                        <li key={heroe.id}>{heroe.superhero}</li>
+                        <HeroCard key={heroe.id} {...heroe} />
                     )
                 })
             }
-        </ul>
+        </div>
     )
 }

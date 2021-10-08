e9551ffec9e8223472546670dea0046d8005c91a
brandonruizmora <brandon_ruizmor@outlook.es>
2021-10-07 23:41:37 -0500

    added heroes list

--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -9,7 +9,7 @@ export const DashboardRoutes = () => {
     return (
         <>
             <Navbar />
-            <div>
+            <div className="container">
                 <Switch>
                     <Route exact path="/marvel" component={ MarvelScreen } />
                     <Route exact path="/dc" component={ DcScreen } />

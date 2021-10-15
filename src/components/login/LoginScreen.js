3129897a798673dd875cdc57bb4d780f268e1e9e
brandonruizmora <brandon_ruizmor@outlook.es>
2021-10-15 14:40:17 -0500

    Login styled

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,33 +1,72 @@
 import React from 'react';
 import { useContext } from 'react';
 import { AuthContext } from '../../auth/AuthContext';
+import { useForm } from '../../hooks/useForm';
 import { types } from '../../types/types';
+import '../../login.css';
 
 export const LoginScreen = ({ history }) => {
 
     const { dispatch } = useContext(AuthContext);
 
+    const [{ userName }, handleInputChange, reset] = useForm({ userName: '' });
+
     const handleLogin = () => {
 
         const lastPath = localStorage.getItem('lastPath') || '/';
 
+        let userNameLogin = 'anonymous';
+
+        if (!userName.length <= 0) {
+            userNameLogin = userName;
+        }
+
         dispatch({
             type: types.login,
             payload: {
-                name: 'Brandon'
+                name: userNameLogin
             }
         });
+        reset();
         history.replace(lastPath);
     }
 
     return (
-        <div className="container mt-5">
-            <h1>Login Screen</h1>
-            <hr />
-            <button 
-            className="btn btn-primary"
-            onClick={ handleLogin }
-            >Login</button>
+        <div className="container">
+            <div className="fill d-flex align-items-center justify-content-center">
+                <div className="card w-50 h-50">
+                    <div className="card-header">
+                        <h1>Login Screen</h1>
+                    </div>
+                    <div className="card-body d-flex align-items-center justify-content-center">
+                        <div className="row">
+                            <div className="col-12">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    autoComplete="off"
+                                    name="userName"
+                                    placeholder="Write an username..."
+                                    onChange={handleInputChange}
+                                    value={userName}
+                                    maxLength="20"
+                                />
+                                <small className="fw-light fst-italic float-end me-1">{userName.length} / 20</small>
+                            </div>
+                            <div className="col-12">
+                                <div className="d-grid gap-1 w-100">
+                                    <button
+                                        className="btn btn-primary mt-4"
+                                        onClick={handleLogin}
+                                    >Login</button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
